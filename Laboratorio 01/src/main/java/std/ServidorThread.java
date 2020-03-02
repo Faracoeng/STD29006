16aeb40a5e27f8815c3ad4e55e8e41369af2bea1
Faracoeng <alfmazucheli@gmail.com>
2020-03-02 15:15:47 -0300

    finalizar depois

--- Laboratorio 01/src/main/java/std/ServidorThread.java
+++ Laboratorio 01/src/main/java/std/ServidorThread.java
@@ -4,13 +4,16 @@ import java.net.Socket;
 
 public class ServidorThread extends Thread {
     private Socket conexao;
-    public ServidorThread(Socket c){this.conexao = c;
-    }public void run(){
+    public ServidorThread(Socket c){
+        this.conexao = c;
+    }
+    public void run(){
         // System.out.println("Thread executada");
         // protocolo :
         // servidor recebe mensagem do cliente
         // servidor responde cliente concatenando a mensagem recebi com uma string "s:"
         // somente finaliza a conexao se clinete mandar a string "fim"
+
 
     }
 }
