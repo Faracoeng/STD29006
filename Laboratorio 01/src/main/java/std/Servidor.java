d9ca4462f75572e72a73dc9fba1fdb116579d734
Faracoeng <alfmazucheli@gmail.com>
2020-03-11 09:41:51 -0300

    codigo servidor comentado

--- Laboratorio 01/src/main/java/std/Servidor.java
+++ Laboratorio 01/src/main/java/std/Servidor.java
@@ -5,10 +5,14 @@ import java.net.Socket;
 
 public class Servidor {
     public static void main(String args[]) throws IOException {
+        // socket aberto na porta 1234
         ServerSocket servidor = new ServerSocket(1234);
         while(true){
+            // servidor aguarda por conexoes de clientes
             Socket conexao = servidor.accept();
+            // Quando se conectar, cria uma Thread para essa conexao
             Thread t = new ServidorThread(conexao);
+            // e dispara ela
             t.start();}
     }
 }
