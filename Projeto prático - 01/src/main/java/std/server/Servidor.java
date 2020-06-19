127f7336b86e8a54908b2e0f53d63c2942425725
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:47:26 -0300

    Definindo Servidor

---
+++ Projeto prático - 01/src/main/java/std/server/Servidor.java
@@ -0,0 +1,8 @@
+package std.server;
+
+// Servidor deverá enviar um pacotes UDP na rede com broadcast
+// contendo seu IP para que as RaspBerry PI consigam se
+// conectar estabelendo conexão TCP
+
+public class Servidor {
+}
