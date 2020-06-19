28c04722774688d3f2310cd9ec03251ee74ee3bb
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 20:01:08 -0300

    funcao de broadcast implementada

--- Projeto prático - 01/src/main/java/std/server/DisparaIP.java
+++ Projeto prático - 01/src/main/java/std/server/DisparaIP.java
@@ -5,6 +5,9 @@ package std.server;
 // servidor na rede idependentemente do fluxo
 // principal do programa através do uso de Threads
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class DisparaIP extends Thread{
@@ -16,5 +19,22 @@ public class DisparaIP extends Thread{
         this.IPServidor = ip;
         this.enderecoBroadcast = endereco;
     }
+
+
+    public void broadcast(String mensagem, InetAddress endereco) throws IOException {
+
+        DatagramSocket socket = new DatagramSocket();
+        // buffer para arazenar IP do servidor e ser enviado no datagrama
+        byte[] buffer = mensagem.getBytes();
+        // enviando pacote UDP para endereços de broadcast na porta 1234,
+        // na qualtodo cliente o estará aguardando ao ser iniciado
+        DatagramPacket pacote = new DatagramPacket(buffer, buffer.length, endereco, 1234);
+        // enviando o pacote
+        socket.setBroadcast(true);
+        socket.send(pacote);
+        socket.close();
+    }
+
+
 
 }
