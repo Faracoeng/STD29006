e475d10729c5da47a3dced992dfdbe9ee83e9dbe
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 20:21:38 -0300

    Rasp pronta para receber broadcast

--- Projeto prático - 01/src/main/java/std/client/Cliente.java
+++ Projeto prático - 01/src/main/java/std/client/Cliente.java
@@ -4,10 +4,17 @@ package std.client;
 // IP do servidor e estabelecerá uma conecção TCP com ele
 
 
+import java.net.DatagramSocket;
+
 public class Cliente {
     public static void main(String[] args) {
         try {
 
+            //Aberto um DatagramSocket no cliente na porta 1234 para receber o broadcast
+            DatagramSocket serverSocket = new DatagramSocket(1234);
+            // Vetor de byte para receber dados do pacote de broadcast
+            byte[] receiveData = new byte[2048];
+            // -----------------------------------------------------------------------
         }catch (Exception e){
             e.printStackTrace();
         }
