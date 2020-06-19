5eb4c7e47e58329a4feeda3c7ac4cbe7db971d98
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:21:41 -0300

    servidor implementado

--- Laboratorio-02/Exercicio/src/main/java/std/server/Servidor.java
+++ Laboratorio-02/Exercicio/src/main/java/std/server/Servidor.java
@@ -4,8 +4,13 @@ package std.server;
 // classeContador, subir umserviço de nomes (rmiregistry)
 // e registrar a instância da classe Contador no serviço de nomes.
 
+import std.InventarioDistribuido;
+
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,7 +24,7 @@ public class Servidor {
     private static final String NOMEOBJDIST = "MeuContador";
 
     public static void main(String[] args) {
-        try{
+        try {
             // recebendo nome do servidor por argumento de linha de comando
             if (args[0] != null) {
                 nomeServidor = args[0];
@@ -28,7 +33,26 @@ public class Servidor {
             if (args[1] != null) {
                 porta = Integer.parseInt(args[1]);
 
-        }catch (RemoteException | AlreadyBoundException ex) {
+                // Criando
+                Inventario c = new Inventario();
+                // Definindo o hostname do servidor
+                System.setProperty("java.rmi.std29006.server.hostname", nomeServidor);
+                InventarioDistribuido stub = (InventarioDistribuido)
+                        UnicastRemoteObject.exportObject(c, 0);
+                // Criando serviço de registro
+                Registry registro = LocateRegistry.createRegistry(porta);
+
+                // Registrando objeto distribuido
+                registro.bind(NOMEOBJDIST, stub);
+
+                System.out.println("Servidor pronto!\n");
+
+                // teste
+                //c.adicionaAP("AP1" ,"mac:abcdef" ,"freq:2.4:5", "Lab SiDi");
+
+                System.out.println("Pressione CTRL + C para encerrar...");
+            }
+        } catch (RemoteException | AlreadyBoundException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
 
