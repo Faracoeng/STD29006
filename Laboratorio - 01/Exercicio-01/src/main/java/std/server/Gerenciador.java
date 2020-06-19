658daa7bb9f9e7d2a3a4d229ef68f83d9e60728b
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 17:28:02 -0300

    Gerenciador implementada

--- Laboratorio - 01/Exercicio-01/src/main/java/std/server/Gerenciador.java
+++ Laboratorio - 01/Exercicio-01/src/main/java/std/server/Gerenciador.java
@@ -1,4 +1,40 @@
 package std.server;
 
+import java.util.HashMap;
+
+//Essa classe deve estruturar toda lista criado em uma HashMap,
+// para associar cada cliente a sua respectiva lista, evita problemas como
+// cliente criar 2 listas com mesmo nome
+
 public class Gerenciador {
+
+    private MinhaLista l;
+    private HashMap<String, MinhaLista> listas;
+
+    public Gerenciador() {
+        listas = new HashMap<>();
+    }
+    // criar uma nova lista,
+    public boolean criarLista(String nomeLista){
+        if(!listas.containsKey(nomeLista)) {
+            listas.put(nomeLista, new MinhaLista(nomeLista));
+            return true;
+        }else return false;
+    }
+    //  adicionar valores em uma lista já existente
+
+    public boolean addValor(String nomeLista, String valor){
+        if(listas.containsKey(nomeLista)){
+            listas.get(nomeLista).setConteudo(valor);
+            return true;
+        }else return false;
+    }
+    //  obter o último valor adicionado em uma lista, e por consequência, removê-lo dessa lista.
+    public String removerValor(String nomeLista){
+        if(listas.containsKey(nomeLista)){
+            return listas.get(nomeLista).obterUltimoValor();
+
+        }else return "erro";
+
+    }
 }
