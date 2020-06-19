a02b2bfd4e96d3d1d02c98fb17d7432a62cbbacc
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 17:28:44 -0300

    IMplementando servidor

--- Laboratorio - 01/Exercicio-01/src/main/java/std/server/Servidor.java
+++ Laboratorio - 01/Exercicio-01/src/main/java/std/server/Servidor.java
@@ -1,4 +1,10 @@
 package std.server;
 
 public class Servidor {
+    public static Gerenciador agenda = new Gerenciador();
+    public static final String MENU = "\n         =========================================================" +
+            "\n        |     1 - Criar nova lista                              |" +
+            "\n        |     2 - adicionar valores em uma lista já existente   |" +
+            "\n        |     3 - remover ultimo valor de uma lista             |" +
+            "\n        |     4 - sair                                          |";
 }
