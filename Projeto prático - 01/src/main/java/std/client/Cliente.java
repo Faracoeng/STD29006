4feba5f7d67b0712476b31065eb257f0bedbe867
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 20:19:51 -0300

    inciando implementacao do cliente/Rasp

--- Projeto prático - 01/src/main/java/std/client/Cliente.java
+++ Projeto prático - 01/src/main/java/std/client/Cliente.java
@@ -1,5 +1,15 @@
 package std.client;
 
-// A classe cliente representa uma RaspBerry, que ouvirá o broadcast contendo o IP do servidor e estabelecerá uma conecção TCP com ele
+// A classe cliente representa uma RaspBerry, que ouvirá o broadcast contendo o
+// IP do servidor e estabelecerá uma conecção TCP com ele
+
+
 public class Cliente {
+    public static void main(String[] args) {
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
