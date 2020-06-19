a4c983619c5d2aef6dba9a327874963f390959be
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:24:45 -0300

    registro

--- Laboratorio-02/Exercicio/src/main/java/std/client/Cliente.java
+++ Laboratorio-02/Exercicio/src/main/java/std/client/Cliente.java
@@ -5,8 +5,12 @@ package std.client;
 // Ao executar o cliente é necessário informar o nome (ou IP) e porta
 // da máquina onde o rmiregistry está sendo executado.
 
+import std.InventarioDistribuido;
+
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +29,12 @@ public class Cliente {
             if (args[1] != null) {
                 porta = Integer.parseInt(args[1]);
             }
+
+            System.out.println("> Conectando no servidor " + nomeServidor);
+            // Obtendo referencia do servico de registro
+            Registry registro = LocateRegistry.getRegistry(nomeServidor, porta);
+            // Procurando pelo objeto distribuido registrado previamente com o NOMEOBJDIST
+            InventarioDistribuido stub = (InventarioDistribuido) registro.lookup(NOMEOBJDIST);
 
 
         } catch (RemoteException | NotBoundException ex) {
