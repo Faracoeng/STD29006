30f30e22a5be1e8b821cf293572cdac0a3198b90
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:22:31 -0300

    iniciando implementação de cliente

---
+++ Laboratorio-02/Exercicio/src/main/java/std/client/Cliente.java
@@ -0,0 +1,9 @@
+package std.client;
+
+
+// A classe Cliente é responsável por invocar os métodos remotos do Servidor.
+// Ao executar o cliente é necessário informar o nome (ou IP) e porta
+// da máquina onde o rmiregistry está sendo executado.
+
+public class Cliente {
+}
