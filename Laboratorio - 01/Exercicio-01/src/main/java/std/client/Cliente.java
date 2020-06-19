9e279af123014b546f4a20bf23fd05b3a08aa9ed
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 17:33:44 -0300

    Interface com o clinete implementada... exercicio finalizado

--- Laboratorio - 01/Exercicio-01/src/main/java/std/client/Cliente.java
+++ Laboratorio - 01/Exercicio-01/src/main/java/std/client/Cliente.java
@@ -5,9 +5,12 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
         int porta = 1234;
         String ip = "127.0.0.1"; // loopback para testar na própria máquina
 
@@ -22,5 +25,31 @@ public class Cliente {
         fluxoSaida.flush();
         String mensagem = fluxoEntrada.readUTF();
         System.out.println("Servidor > " + mensagem);
+
+//-----------------------------------------------------------------------------
+        /* Inicia comunicacao com menu*/
+        String resposta = "";
+        do {
+            System.out.println(fluxoEntrada.readUTF()); // mensagem do servidor
+            resposta = teclado.nextLine();
+            // resposta cliente
+
+            fluxoSaida.writeUTF(resposta);
+            //System.out.println(opcao);
+            fluxoSaida.flush();
+            // Quando se invoca o flush(), significa enviar todos os conteúdos naquele momento.
+            // ele é chamado implicitamente, não precisa realmente invocar esse método em todas as situações,
+            // em ambiente simples provavelmente sempre vai funcionar sem o flush, agora em um ambiente mais complexo,
+            // talvez os dados se percam no meio do caminho.
+            // System.out.println(fluxoEntrada.readUTF());
+        }while (!resposta.equals("4"));
+
+        //System.out.println(fluxoEntrada.readUTF());
+//-----------------------------------------------------------------------------
+        /* Fecha fluxos e socket */
+        fluxoSaida.close();
+        fluxoEntrada.close();
+        conexao.close();
+
     }
 }
