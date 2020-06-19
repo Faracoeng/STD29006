7ab331de56db498bce0ced73c4c3aed28302f970
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 17:53:12 -0300

    escopo de ServidorThread implementado

--- Laboratorio - 01/Exercicio-02/src/main/java/std/server/ServidorThread.java
+++ Laboratorio - 01/Exercicio-02/src/main/java/std/server/ServidorThread.java
@@ -1,4 +1,14 @@
 package std.server;
 
+import java.net.Socket;
+
 public class ServidorThread {
+        private Socket conexao;
+
+        public ServidorThread(Socket c){
+            this.conexao = c;
+        }
+        public void run(){
+            System.out.println("Thread executada");
+        }
 }
