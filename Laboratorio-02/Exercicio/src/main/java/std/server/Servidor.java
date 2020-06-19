aa863722c7117caceff832965b6ce8363e4c2b94
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:17:10 -0300

    iniciando implementação do Servidor

--- Laboratorio-02/Exercicio/src/main/java/std/server/Servidor.java
+++ Laboratorio-02/Exercicio/src/main/java/std/server/Servidor.java
@@ -1,4 +1,8 @@
 package std.server;
 
+// A classe Servidor é responsável por instanciar um objeto da
+// classeContador, subir umserviço de nomes (rmiregistry)
+// e registrar a instância da classe Contador no serviço de nomes.
+
 public class Servidor {
 }
