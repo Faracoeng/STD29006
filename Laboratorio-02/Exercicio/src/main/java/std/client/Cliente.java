6e2d6f16d7bd8165e3c4795490fe3771aaf42f6e
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:24:13 -0300

    tratando argumentos por linha de comando

--- Laboratorio-02/Exercicio/src/main/java/std/client/Cliente.java
+++ Laboratorio-02/Exercicio/src/main/java/std/client/Cliente.java
@@ -5,9 +5,31 @@ package std.client;
 // Ao executar o cliente é necessário informar o nome (ou IP) e porta
 // da máquina onde o rmiregistry está sendo executado.
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Cliente {
 
     private static String nomeServidor;
     private static int porta = 12345;
     private static final String NOMEOBJDIST = "MeuInventario";
+
+    public static void main(String[] args) {
+        try {
+            // recebendo nome do servidor por argumento de linha de comando
+            if (args[0] != null) {
+                nomeServidor = args[0];
+            }
+            if (args[1] != null) {
+                porta = Integer.parseInt(args[1]);
+            }
+
+
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
 }
