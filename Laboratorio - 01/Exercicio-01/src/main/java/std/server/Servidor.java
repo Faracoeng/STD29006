c2b038b2a4bff425a5111f49a2cec61176fbb024
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 17:30:12 -0300

    estabelecimento de conexão com o servidor implementado

--- Laboratorio - 01/Exercicio-01/src/main/java/std/server/Servidor.java
+++ Laboratorio - 01/Exercicio-01/src/main/java/std/server/Servidor.java
@@ -1,5 +1,12 @@
 package std.server;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
     public static Gerenciador agenda = new Gerenciador();
     public static final String MENU = "\n         =========================================================" +
@@ -7,4 +14,23 @@ public class Servidor {
             "\n        |     2 - adicionar valores em uma lista já existente   |" +
             "\n        |     3 - remover ultimo valor de uma lista             |" +
             "\n        |     4 - sair                                          |";
+
+    public static void main(String[] args) throws IOException {
+        /* Registra servico na porta 1234 e aguarda por conexoes */
+        ServerSocket servidor = new ServerSocket(1234);
+
+        System.out.println("Aguardando por conexoes");
+
+        Socket conexao = servidor.accept();
+        System.out.println("Cliente conectou " + conexao);
+
+        /* Estabelece fluxos de entrada e saida */
+        DataInputStream fluxoEntrada = new DataInputStream(new BufferedInputStream(conexao.getInputStream()));
+        DataOutputStream fluxoSaida = new DataOutputStream(conexao.getOutputStream());
+
+        /* inicia a comunicacao */
+        String mensagem = fluxoEntrada.readUTF();
+        System.out.println("Cliente > " + mensagem);
+        fluxoSaida.writeUTF("Oi, eu sou o servidor!");
+    }
 }
