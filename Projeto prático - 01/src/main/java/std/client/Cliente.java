78ed5614e385e1a21efb73516bd7f25fec0a48ef
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:43:00 -0300

    iniciando Definindo cliente

---
+++ Projeto prático - 01/src/main/java/std/client/Cliente.java
@@ -0,0 +1,5 @@
+package std.client;
+
+// A classe cliente representa uma RaspBerry, que ouvirá o broadcast contendo o IP do servidor e estabelecerá uma conecção TCP com ele
+public class Cliente {
+}
