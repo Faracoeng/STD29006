3505617b7b85bb3d08f7f9093939f86b2dbd75e2
Faracoeng <alfmazucheli@gmail.com>
2020-06-19 19:51:07 -0300

    obtendo IP da máquina com aplicativo servidor

--- Projeto prático - 01/src/main/java/std/server/Servidor.java
+++ Projeto prático - 01/src/main/java/std/server/Servidor.java
@@ -4,5 +4,37 @@ package std.server;
 // contendo seu IP para que as RaspBerry PI consigam se
 // conectar estabelendo conexão TCP
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 public class Servidor {
+
+    // descobre ip da máquina com aplicativo servidor
+    static public String getIPV4Semlo() {
+        // NetworkInterface.getNetworkInterfaces() retorna Enumeration<NetworkInterface>
+        String elementos = "";
+        Enumeration nis = null;
+        try {
+            nis = NetworkInterface.getNetworkInterfaces(); // retorna todas as interfaces de rede da máquina para
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        while (nis.hasMoreElements()) {
+            NetworkInterface ni = (NetworkInterface) nis.nextElement();
+            // procura uma interface de rede que tenha o endereço IP especificado.
+            Enumeration ias = ni.getInetAddresses();
+            while (ias.hasMoreElements()) {
+                InetAddress ia = (InetAddress) ias.nextElement();
+                if (!ni.getName().equals("lo")) // remove interface de loopback
+                    elementos = ia.getHostAddress();
+            }
+        }
+        return elementos;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ Projeto prático - 01/src/main/java/std/server/ServidorThread.java
@@ -0,0 +1,10 @@
+package std.server;
+
+
+// Como servidor deve atender separadamente 4 Raspberry
+// Decidi fazer um server MultThread
+
+public class ServidorThread {
+
+
+}
