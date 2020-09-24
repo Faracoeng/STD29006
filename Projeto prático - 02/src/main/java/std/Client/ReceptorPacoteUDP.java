fa172ae316cde3375206b9b91e2c1650534198e3
Faracoeng <alfmazucheli@gmail.com>
2020-09-24 08:49:34 -0300

    Atributos definidos

--- Projeto prático - 02/src/main/java/std/Client/ReceptorPacoteUDP.java
+++ Projeto prático - 02/src/main/java/std/Client/ReceptorPacoteUDP.java
@@ -1,4 +1,14 @@
 package std.Client;
 
+import java.io.DataOutputStream;
+import java.util.ArrayList;
+
 public class ReceptorPacoteUDP {
+
+    private ArrayList<String[]> listaRecebida;
+    private int tempoRecebido;
+    private static ProcessoWorker obj;
+    private String idDoWorker;
+    public boolean terminei = false;
+    DataOutputStream fluxoSaida;
 }
