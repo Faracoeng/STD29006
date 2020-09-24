e07ab86ffd1a43cfdf0d77a5c2dd1baf4f51412f
Faracoeng <alfmazucheli@gmail.com>
2020-09-24 08:53:26 -0300

    Thread implementada

--- Projeto prático - 02/src/main/java/std/Client/ReceptorPacoteUDP.java
+++ Projeto prático - 02/src/main/java/std/Client/ReceptorPacoteUDP.java
@@ -11,4 +11,34 @@ public class ReceptorPacoteUDP extends Thread {
     private String idDoWorker;
     public boolean terminei = false;
     DataOutputStream fluxoSaida;
+
+    @Override
+    public void run() {
+
+        int i = 0;
+        for (String[] evento : this.listaRecebida) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //System.out.println("Leu do arquivo: " + evento[0]+"_"+evento[1]);
+            obj.adicionarEventoNaFila(evento[0]+"_"+evento[1]);
+            // quando tiver processado a ultima linha dira que terminou
+            if (i == this.listaRecebida.size() - 1) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    this.fluxoSaida.writeUTF("terminei");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            i++;
+        }
+    }
+
 }
